00d020d76ddc580568ed2a21fd41513b05115702
Lambor <757016515@qq.com>
2015-05-21 18:20:16 +0800

    Update loudness_1100600844_servo.ino

--- loudness_1100600844_servo/loudness_1100600844_servo.ino
+++ loudness_1100600844_servo/loudness_1100600844_servo.ino
@@ -22,7 +22,9 @@
 * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-
+#include <Wire.h>
+#include "VisualScope.h"
+#include "TimerOne.h"
 
 //Watchdog setting
 #include <avr/wdt.h>
@@ -31,6 +33,7 @@
 unsigned long resetTime = 0;
 volatile bool  flg_power = 0;
 void(* resetFunc) (void) = 0;  
+
 void watchdogSetup()
 {
     cli();  
@@ -44,12 +47,12 @@ ISR(WDT_vect)
 { 
   if(millis() - resetTime > TIMEOUTPERIOD){
     doggieTickle();                                          
-	resetFunc();     
+    resetFunc();     
   }
 }
 
 //DeBug  switch 
-#define  DeBug   0
+#define  DeBug   1
 
 #include <Servo.h> 
 // Servo position begin value
@@ -60,14 +63,11 @@ ISR(WDT_vect)
 // Sound sensor pin input define
 const int pin_sound = A5;
 
+//sound analog value
+int val_sound = 0;
+
 // Create servo object to control a servo
 Servo myservo;   
-
-// Analog pin used to connect the potentiometer
-int potpin = A5;  
-
-// Variable to read the value from the analog pin 
-int val;    
 
 const long interval = 50;
 unsigned long previousMillis = 0;
@@ -75,103 +75,102 @@ unsigned long currentMillis  = 0;
 
 #define  interval_val   100
 
-int table[9]= {1,2,3,4,5,6,7,8,9};
-
-// =========  Setup  =========
 void setup()
 {
-//==============================//
-	watchdogSetup();
-	pinMode (10,OUTPUT);
-	for(int i=0;i<2;i++)
-	{
-	digitalWrite(10,HIGH);
-	delay(500);
-	digitalWrite(10,LOW); 		
-	delay(500);	
-	doggieTickle();
-	}
-	//while(1);   //debug  watchdog 	
-#if DeBug	
-    Serial.begin(9600);
-	Serial.println("start");
-#endif  	
-//==============================//	
+/*============ Watch Dog ============*/
+  watchdogSetup();
+  pinMode (10,OUTPUT);
+  for(int i=0;i<2;i++)
+  {
+    digitalWrite(10,HIGH);
+    delay(500);
+    digitalWrite(10,LOW);     
+    delay(500);  
+    doggieTickle();
+  } 
+  //while(1);   //debug  watchdog   
+#if DeBug  
+  Serial.begin(9600);
+  Serial.println("start");
+#endif    
+//============ end ===========//  
 
+  myservo.attach(3);  //    mini fan use pin 9   joint use pin 3
+  previousMillis = millis();
+  myservo.write(90); 
 
-	myservo.attach(3);  //    mini fan use pin 9   joint use pin 3
-	previousMillis = millis();
-	myservo.write(90); 
 }
 
 // =========  Loop  =========
 void loop()
-{	
-	currentMillis = millis();
-	if(currentMillis - previousMillis > interval)
- 	{	
-	    doggieTickle();     
-
-		int val_sound = (analogRead(pin_sound));
-		Serial.println (val_sound);
-
-		if ((val_sound > 150)&&(val_sound < 200))
-		{		 		
-			myservo.write(pos_begin - 10);   
-			delay_feed(5*10);
-			myservo.write(pos_end + 10);  	
-			delay_feed(5*10);
-		}	
-                
-		if ((val_sound > 200)&&(val_sound < 300))
-		{		 
-			myservo.write(pos_begin);   
-			delay_feed(5*20);
-			myservo.write(pos_end);  	
-			delay_feed(5*20);
-		}	
-                
-		if ((val_sound > 300)&&(val_sound < 350))
-		{		 		
-			myservo.write(pos_begin - 10);   
-			delay_feed(5*40);
-			myservo.write(pos_end + 10);  	
-			delay_feed(5*40);
-		}
-                	
-		if ((val_sound > 350)&&(val_sound < 400))
-		{		 	
-			myservo.write(pos_begin - 20);   
-			delay_feed(5*60);
-			myservo.write(pos_end + 20);  	
-			delay_feed(5*60);
-		}
-                	
-		if ((val_sound > 400)&&(val_sound < 450))
-		{		 		
-			myservo.write(pos_begin - 30);   
-			delay_feed(5*80);
-			myservo.write(pos_end + 30);  	
-			delay_feed(5*80);
-		}
-		previousMillis = millis();
-	
-	delay(80);
-	doggieTickle(); 
-	}	
+{  
+  currentMillis = millis();
+  
+  if(currentMillis - previousMillis > interval)
+  {  
+    doggieTickle();                 
+    val_sound = average_filter(pin_sound, 200); 
+    Data_acquisition(val_sound,0,0,0);
+    //Serial.println (val_sound);
+    if ((val_sound > 150)&&(val_sound < 200))
+    {         
+      myservo.write(pos_begin - 10);   
+      delay_feed(5*10);
+      myservo.write(pos_end + 10);    
+      delay_feed(5*10);
+    }              
+    else if ((val_sound > 200)&&(val_sound < 300))
+    {     
+      myservo.write(pos_begin);   
+      delay_feed(5*20);
+      myservo.write(pos_end);    
+      delay_feed(5*20);
+    }             
+    else if ((val_sound > 300)&&(val_sound < 350))
+    {         
+      myservo.write(pos_begin - 10);   
+      delay_feed(5*40);
+      myservo.write(pos_end + 10);    
+      delay_feed(5*40);
+    }                  
+	  else if ((val_sound > 350)&&(val_sound < 400))
+    {       
+      myservo.write(pos_begin - 20);   
+      delay_feed(5*60);
+      myservo.write(pos_end + 20);    
+      delay_feed(5*60);
+    }                 
+    else if ((val_sound > 400)&&(val_sound < 450))
+    {         
+      myservo.write(pos_begin - 30);   
+      delay_feed(5*80);
+      myservo.write(pos_end + 30);    
+      delay_feed(5*80);
+    }
+    previousMillis = millis();
+  
+    //delay(80);
+    doggieTickle(); 
+  }  
 }
 
 // Delay with feed dog
 void delay_feed( int val)
 {
        delay(val);
-	   doggieTickle(); 
+     doggieTickle(); 
 
 }
 
-
-
-
+int average_filter(int analog_pin, int num)
+{
+  long temp = 0;
+  for(int i=0;i<num;i++)
+  {
+    temp += analogRead(analog_pin);    
+  }
+  return temp/num;
+}
 
 
 

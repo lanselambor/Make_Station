400713247af3dcffb6bec838656082efb8f396ff
lanselambor <757016515@qq.com>
2015-05-31 14:35:17 +0800

    update

--- ultrasonic_110060075_speaker/ultrasonic_110060075_speaker.ino
+++ ultrasonic_110060075_speaker/ultrasonic_110060075_speaker.ino
@@ -21,16 +21,27 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
-#include "WatchDog.h"
-#include "Ultrasonic.h"
-#include <Wire.h>
 
-#define  DeBug  0
+#include "WatchDog.h"
+#include <Ultrasonic.h>
+
+#define  DeBug   0
+
+#define BUTTON         2
+#define LIGHT_SENSOR   A0
+#define CHRG_LED       A3  //low-level work
+#define PWR_HOLD       A1  
+#define PWR            6   //low-level work
+#define KEY            2
+#define LED            10  
+#define OUT_PIN1       3   //normal output pin
+#define OUT_PIN2       5
+#define IN_PIN1        A5  //normal input pin
+#define IN_PIN2        A4
 
 #define PWM_OUT_PIN       3
 #define ULTRASONIC_PIN    A5
 #define DEFAULT_DISTANCE  5
-#define FILTER_ZONE       1
 
 #define DISTANCE1         DEFAULT_DISTANCE * 1
 #define DISTANCE2         DEFAULT_DISTANCE * 2
@@ -44,111 +55,99 @@
 
 Ultrasonic ultrasonicAir(ULTRASONIC_PIN);
 unsigned int noteDurations = 50 ;
-boolean playBack = false;
-
 
 void setup()
 { 
 
-	WTD.watchdogSetup();
-	WTD.doggieTickle();
-	
-	pinMode (10,OUTPUT);
-	for(int i=0;i<2;i++)
-	{
-		digitalWrite(10,HIGH);
-		delay(500);
-		digitalWrite(10,LOW); 		
-		delay(500);	
-		WTD.doggieTickle();
-	}
-	//while(1);   //debug  watchdog 	
-#if DeBug	
-    Serial.begin(9600);
-	Serial.println("start");
-#endif  	
+  WTD.watchdogSetup();
+  WTD.doggieTickle();
+  
+  pinMode(CHRG_LED, OUTPUT);  //open power led 
+  digitalWrite(CHRG_LED, LOW);
+  
+  //The shining led
+  pinMode (10,OUTPUT);   
+  for(int i=0;i<2;i++)
+  {
+    analogWrite(10,10);  //10 is pwm output duty/256
+    delay(500);
+    analogWrite(10,0);     
+    delay(500);  
+    WTD.doggieTickle();
+  }  
+#if DeBug  
+  Serial.begin(9600);
+  Serial.println("start");
+#endif    
 
 }
 
+
 void loop()
 {
-    volatile unsigned char airGamutValue = 0;
-    volatile unsigned int ultrasonicAirDistance = 0;
-	
-    WTD.doggieTickle(); 
-	
-	
-    while(1)
-    {                
-        ultrasonicAirDistance = ultrasonicAir.MeasureInCentimeters(); 
-        
-        if     ((ultrasonicAirDistance>DISTANCE1)&&(ultrasonicAirDistance<=DISTANCE2))airGamutValue = 1, playBack = true;
-        else if((ultrasonicAirDistance>DISTANCE2)&&(ultrasonicAirDistance<=DISTANCE3))airGamutValue = 2, playBack = true;
-        else if((ultrasonicAirDistance>DISTANCE3)&&(ultrasonicAirDistance<=DISTANCE4))airGamutValue = 3, playBack = true;
-        else if((ultrasonicAirDistance>DISTANCE4)&&(ultrasonicAirDistance<=DISTANCE5))airGamutValue = 4, playBack = true;
-        else if((ultrasonicAirDistance>DISTANCE5)&&(ultrasonicAirDistance<=DISTANCE6))airGamutValue = 5, playBack = true;
-        else if((ultrasonicAirDistance>DISTANCE6)&&(ultrasonicAirDistance<=DISTANCE7))airGamutValue = 6, playBack = true;
-        else if((ultrasonicAirDistance>DISTANCE7)&&(ultrasonicAirDistance<=DISTANCE8))airGamutValue = 7, playBack = true;
-        else if((ultrasonicAirDistance>DISTANCE8)&&(ultrasonicAirDistance<=DISTANCE9))airGamutValue = 8, playBack = true;
-        else airGamutValue = 0, playBack = false;
-		
-		Gamut_Play(airGamutValue);
-#if DeBug
-		Serial.print("Ult = ");
-        Serial.println(ultrasonicAirDistance);
-#endif		    				
-		
+  unsigned char airGamutValue = 0;
+  unsigned int ultrasonicAirDistance = 0;
+
+  WTD.doggieTickle(); 
+    
+  while(1)
+  {                
+    ultrasonicAirDistance = ultrasonicAir.MeasureInCentimeters(); 
+    
+    if     ((ultrasonicAirDistance>DISTANCE1)&&(ultrasonicAirDistance<=DISTANCE2))airGamutValue = 1;
+    else if((ultrasonicAirDistance>DISTANCE2)&&(ultrasonicAirDistance<=DISTANCE3))airGamutValue = 2;
+    else if((ultrasonicAirDistance>DISTANCE3)&&(ultrasonicAirDistance<=DISTANCE4))airGamutValue = 3;
+    else if((ultrasonicAirDistance>DISTANCE4)&&(ultrasonicAirDistance<=DISTANCE5))airGamutValue = 4;
+    else if((ultrasonicAirDistance>DISTANCE5)&&(ultrasonicAirDistance<=DISTANCE6))airGamutValue = 5;
+    else if((ultrasonicAirDistance>DISTANCE6)&&(ultrasonicAirDistance<=DISTANCE7))airGamutValue = 6;
+    else if((ultrasonicAirDistance>DISTANCE7)&&(ultrasonicAirDistance<=DISTANCE8))airGamutValue = 7;
+    else if((ultrasonicAirDistance>DISTANCE8)&&(ultrasonicAirDistance<=DISTANCE9))airGamutValue = 8;
+    else airGamutValue = 0;
+    
+    if(airGamutValue>0)
+    {
 #if DeBug             
-		Serial.print("Air Gamut Value is:"); 
-		Serial.println(airGamutValue); 
-#endif             	
-			
-        delay(10);       
-		WTD.doggieTickle();   
+      Serial.print("Air Gamut Value is:"); 
+      Serial.println(airGamutValue); 
+#endif             
+      Gamut_Play(airGamutValue);
     }
+    delay(30);      
+    WTD.doggieTickle();   
+  }
 }
 
 void Gamut_Play(unsigned char data)
 {
-    if(playBack)
-	{
-		switch(data)
-		{      
-			case 1:
-				tone(PWM_OUT_PIN,523);//Do(523Hz)NOTE_C5
-				break;        
-			case 2:
-				tone(PWM_OUT_PIN,587);//Re(587Hz)NOTE_D5
-				break;        
-			case 3:
-				tone(PWM_OUT_PIN,659);//Mi(659Hz)NOTE_E5
-				break;        
-			case 4:
-				tone(PWM_OUT_PIN,698);//Fa(698Hz)NOTE_F5
-				break;        
-			case 5:
-				tone(PWM_OUT_PIN,784);//So(784Hz)NOTE_G5
-				break;        
-			case 6:
-				tone(PWM_OUT_PIN,880);//La(880Hz)NOTE_A5
-				break;       
-			case 7:
-				tone(PWM_OUT_PIN,988);//Si(988Hz)NOTE_B5
-				break;       
-			case 8:
-				tone(PWM_OUT_PIN,1047);//Do(1047Hz)NOTE_C6
-				break;             
-			default:
-				noTone(PWM_OUT_PIN);
-				break;         
-		}
-	}
-	else 
-	{
-		noTone(PWM_OUT_PIN);
-	}
-	
+  switch(data)
+  {      
+    case 1:
+      tone(PWM_OUT_PIN,523,noteDurations);//Do(523Hz)NOTE_C5
+      break;        
+    case 2:
+      tone(PWM_OUT_PIN,587,noteDurations);//Re(587Hz)NOTE_D5
+      break;        
+    case 3:
+      tone(PWM_OUT_PIN,659,noteDurations);//Mi(659Hz)NOTE_E5
+      break;        
+    case 4:
+      tone(PWM_OUT_PIN,698,noteDurations);//Fa(698Hz)NOTE_F5
+      break;        
+    case 5:
+      tone(PWM_OUT_PIN,784,noteDurations);//So(784Hz)NOTE_G5
+      break;        
+    case 6:
+      tone(PWM_OUT_PIN,880,noteDurations);//La(880Hz)NOTE_A5
+      break;       
+    case 7:
+      tone(PWM_OUT_PIN,988,noteDurations);//Si(988Hz)NOTE_B5
+      break;       
+    case 8:
+      tone(PWM_OUT_PIN,1047,noteDurations);//Do(1047Hz)NOTE_C6
+      break;             
+    default:
+      break;         
+  }
 }
-
 
 

4d0a76cebd26fb4d7afe22512c05f4fbfa6653e5
lanselambor <757016515@qq.com>
2015-06-09 14:29:21 +0800

    update

--- loudness_110060095_motor/loudness_110060095_motor.ino
+++ loudness_110060095_motor/loudness_110060095_motor.ino
@@ -1,10 +1,10 @@
 /*
-* Groot.ino
-* A demo for ChaiHuo ZaoWuBa Demo T14006
+* Digital Sand Clock.ino
+* A demo for ChaiHuo ZaoWuBa Demo
 * 
 * Copyright (c) 2015 Seeed Technology Inc.
-* Auther     : Jacob.Yan
-* Create Time: Jan 07, 2015
+* Auther     : Lambor.Fang
+* Create Time: May 2015
 * Change Log :
 * 
 * This library is free software; you can redistribute it and/or
@@ -21,12 +21,11 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
+#include <TimerOne.h>
 #include "WatchDog.h"
-#include "VisualScope.h"
 #include <Wire.h>
-#include <Servo.h>
- 
-#define  DEBUG   1
+
+#define DEBUG 0   //enable debug
 
 #define BUTTON         2
 #define LIGHT_SENSOR   A0
@@ -40,65 +39,31 @@
 #define IN_PIN1        A5  //normal input pin
 #define IN_PIN2        A4
 
-#if DEBUG
-VisualScope vs;
-#endif
+#define MOTOR_SPEED 5
+#define THRESHOLD 30
 
+int sound_pin = A5;
+int motor = 3;
+volatile int sound_value = 0;
+volatile int last_value = 0;
+volatile bool Motor_Sleep = false;
+volatile bool motor_run_flag = false;
 
-
-#define original_pos 90 
-// Servo position begin value
-#define pos_begin  80
-// Servo position end value
-#define pos_end    100
-// Create servo object to control a servo
-Servo myservo;
-
-// Sound sensor pin input define
-const int pin_sound = A5;
-
-//sound analog value
-int quiet_value = 0; 
-int val_sound = 0;
-
-//value off the thershold, 
-int thershold_off = 50;
-//sound threshold
-int Threshold[5] = {40 + thershold_off, 
-                    60 + thershold_off, 
-                    80 + thershold_off,
-                    100 + thershold_off, 
-                    120 + thershold_off};
-   
-
-const long interval = 50;
-unsigned long previousMillis = 0;
-unsigned long currentMillis  = 0;
-
-int midNum(int *a, int *b, int *c);
-int average_filter(int analog_pin, int num);
-int mid_filter(int analog_pin);
-void delay_feed( int val);
-
-void setup()
+void setup() 
 { 
+  //initial WatchDog
+  WTD.watchdogSetup();
+  WTD.doggieTickle();
+  
   //power up
   pinMode(CHRG_LED, OUTPUT);
   digitalWrite(CHRG_LED, LOW);
-  
-  //initial watchdog  
-  WTD.watchdogSetup();
-  WTD.doggieTickle();
-
-  //get quiet sound value
-  delay(500);
-  long tmp = 0;
-  for(int i = 0;i<1000;i++)
-  {
-    tmp += analogRead(pin_sound);
-  }
-  quiet_value = tmp / 1000;
-
+   
+  Serial.begin(9600);
+  pinMode(sound_pin, INPUT);
+  pinMode(motor,OUTPUT);
+ 
+  //start light
   pinMode (10,OUTPUT);
   for(int i=0;i<2;i++)
   {
@@ -107,96 +72,43 @@ void setup()
     analogWrite(10,0);     
     delay(500);  
     WTD.doggieTickle();
-  } 
-
-  #if DEBUG  
-  Serial.begin(9600);
-  Serial.println("start");
-  #endif    
-
-  myservo.attach(3);  //    mini fan use pin 9   joint use pin 3
-  previousMillis = millis();
-  myservo.write(90); 
-
-}
-
-void loop()
-{  
-  WTD.doggieTickle();                 
-  val_sound = average_filter(pin_sound, 50);       
-  int threshold = val_sound - quiet_value;
-  if(threshold > thershold_off)
-  {
-    Serial.println(threshold);
-    servoRun(threshold);
-  }              
-  WTD.doggieTickle();   
-  delay(200);  
-}
-
-void servoRun(int analog)
-{
-  int dec = 0;
-  if(analog < 200)
-  {  
-    dec = map(analog, thershold_off, 200, 2, 30);
   }
-  else
-  {
-    dec = 30;
-  }
-  myservo.write(original_pos - dec);
-  delay(200);
-  myservo.write(original_pos + dec);
-  delay(200);
-}
-// Delay with feed dog
-void delay_feed( int val)
-{
-  delay(val);
-  WTD.doggieTickle(); 
-}
-
-int mid_filter(int analog_pin)
-{
-  int a = analogRead(analog_pin);
-  delayMicroseconds(10);
-  int b = analogRead(analog_pin);
-  delayMicroseconds(10);
-  int c = analogRead(analog_pin);
-  delayMicroseconds(10);
   
-  return midNum(&a, &b, &c);
+  Timer1.initialize(50000);//timing for 50ms
+  Timer1.attachInterrupt(TimingISR);//declare the interrupt serve routine:TimingISR  
 }
 
-int average_filter(int analog_pin, int num)
-{
-  long temp = 0;
-  for(int i=0;i<num;i++)
+void TimingISR(void)
+{   
+  WTD.doggieTickle();
+  sound_value = analogRead(sound_pin);
+  if(40 < (sound_value - last_value))
   {
-    //temp += analogRead(analog_pin);    
-    temp += mid_filter(analog_pin);    
+    motor_run_flag = true;
   }
-  return temp/num;
+  last_value = sound_value;
 }
 
-int midNum(int *a, int *b, int *c)
+void loop() 
 {
-  int tmp = 0;
-  if(*a > *b){
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+  if(motor_run_flag)
+  {
+    motorRun();
   }
-  if(*b > *c){
-    tmp = *b;
-    *b = *c;
-    *c = tmp;
+  WTD.doggieTickle();
+  delay(500);
+}
+
+void motorRun()
+{
+  motor_run_flag = false;
+  WTD.doggieTickle();
+  for(int i=0; i<100; i++)
+  {
+    WTD.doggieTickle();
+    analogWrite(motor, 10);
+    delay(10);
+    digitalWrite(motor, 0);  
+    delay(10);
   }
-  if(*b > *c){
-    tmp = *b;
-    *b = *c;
-    *c = tmp;
-  }
-  return *b;
 }

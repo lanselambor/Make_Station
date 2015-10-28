a9b3a8cb5bc03fc1c4c68b4658991bcae0ca2424
Lambor <757016515@qq.com>
2015-10-28 13:44:43 +0800

    Update switch_110060103_vibration.ino

--- joint2.0BatteryManager/switch_110060103_vibration/switch_110060103_vibration.ino
+++ joint2.0BatteryManager/switch_110060103_vibration/switch_110060103_vibration.ino
@@ -41,7 +41,7 @@
 
 #define SWITCH         A5
 #define VIBRATOR       3
-#define TIMEUP         60//1*60  //45 minutes
+#define TIMEUP         45*60  //45 minutes
 
 unsigned long timeCounter = 0;
 
